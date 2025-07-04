7ce9db59205ac8c9c67c48db15c4e69cb47f4803
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-04 15:02:08 -0300

    Update 1028.c

--- 1028.c
+++ 1028.c
@@ -1,46 +1,7 @@
 #include <stdio.h>
-int MDC(int a, int b) {
-    int resto;
-    while (b != 0) {
-        resto = a % b;
-        a = b;
-        b = resto;
-    }
-    return b;
-}
+
+int mdc(int a, int b) {
+    
+
 
 int main() {
-    int N;
-    int F1, F2;
-    scanf("%d", &N);
-    for (int i = 0; i < N; i++) {
-        scanf("%d %d", &F1, &F2);
-        if (F1 > 0 && F2 > 0) {
-            if (F1 = F2) {
-                printf("%d\n", F1);
-            }
-            if (F1 < F2) {
-                printf("%d\n", MDC(F2, F1));
-            }
-            if (F1 > F2) {
-                printf("%d\n", MDC(F1, F2));
-            }
-        }
-        if 
-    }
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
