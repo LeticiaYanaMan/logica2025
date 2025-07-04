925241efd7185ca0ed61e4985364859d39473621
LeticiaYanaMan <ly.manca30@unifesp.br>
2025-07-04 15:15:49 -0300

    Update 1028.c

--- 1028.c
+++ 1028.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int mdc(int a, int b) {
+    if (b = 0) {
+        return a;
+    }
+    return mdc(b, a % b);
+}
     
-
-
 int main() {
+    int N;
+    int F1, F2;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        scanf("%d %d", &F1, &F2);
+        
